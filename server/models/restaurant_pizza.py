33ce8730ec716bd22cab911cfb88e7187b5fa0eb
Ernest Kamau <1.kamauernest@gmail.com>
2025-06-16 11:29:00 +0300

    add RestaurantPizza  models

---
+++ server/models/restaurant_pizza.py
@@ -0,0 +1,22 @@
+from sqlalchemy_serializer import SerializerMixin
+from . import db
+
+
+
+class RestaurantPizza(db.Models, SerializerMixin):
+    __tablename__ = 'restaurant_pizzas'
+    serialize_rules = ('-restaurant.restaurant_pizzas', '-pizza.restaurant_pizzas',)
+    
+    id = db.Column(db.Integer, primary_key=True)
+    price = db.Column(db.Integer, nullable=False)
+    
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurants.id'))
+    pizza_id = db.Column(db.Integer, db.ForeignKey('pizzas.id'))
+   
+    restaurant = db.relationship('Restaurant', back_populates='restaurant_pizzas')
+    pizza = db.relationship('Pizza', back_populates='restaurant_pizzas')
+    
+    def __repr__(self):
+        return f"<id={self.id}, price={self.price}, restaurant_id={self.restaurant_id}, pizza_id={self.pizza_id}>"
+    
+    
