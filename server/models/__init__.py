6dc335337599f644aa11a1eec3a8b9412392fc45
Ernest Kamau <1.kamauernest@gmail.com>
2025-06-16 09:40:15 +0300

    create flask app

---
+++ server/models/__init__.py
@@ -0,0 +1,7 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
+from sqlalchemy_serializer import SerializerMixin
+
+metadata = MetaData()
+
+db = SQLAlchemy(metadata=metadata)
