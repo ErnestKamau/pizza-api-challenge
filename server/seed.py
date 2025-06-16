10056f94f28b00851f22e645657c221c4cd1f393
Ernest Kamau <1.kamauernest@gmail.com>
2025-06-16 15:51:58 +0300

    Add seed data

--- server/seed.py
+++ server/seed.py
@@ -17,6 +17,8 @@ with app.app_context():
     r1 = Restaurant(name="Mama's Pizza", address="123 Main St")
     r2 = Restaurant(name="Pizza Palace", address="456 Side St")
     r3 = Restaurant(name="Neapolitan Express", address="789 Elm St")
+    r4 = Restaurant(name="Joe's Pizza", address="592 Kingston St")
+    r5 = Restaurant(name="GG's Italian Pizzeria", address="916 Pine Avenue")
 
     # Create pizzas
     p1 = Pizza(name="Margherita", ingredients="Tomato, Mozzarella, Basil")
@@ -24,7 +26,7 @@ with app.app_context():
     p3 = Pizza(name="Hawaiian", ingredients="Tomato, Mozzarella, Ham, Pineapple")
 
     # Add them to the session
-    db.session.add_all([r1, r2, r3, p1, p2, p3])
+    db.session.add_all([r1, r2, r3, r4, r5, p1, p2, p3])
     db.session.commit()
 
     # Create restaurant-pizza relationships with prices
@@ -33,8 +35,9 @@ with app.app_context():
     rp3 = RestaurantPizza(price=11, restaurant_id=r2.id, pizza_id=p2.id)
     rp4 = RestaurantPizza(price=14, restaurant_id=r2.id, pizza_id=p3.id)
     rp5 = RestaurantPizza(price=13, restaurant_id=r3.id, pizza_id=p1.id)
+    rp6 = RestaurantPizza(price=20, restaurant_id=r5.id, pizza_id=p3.id)
 
-    db.session.add_all([rp1, rp2, rp3, rp4, rp5])
+    db.session.add_all([rp1, rp2, rp3, rp4, rp5, rp6])
     db.session.commit()
 
     print("Done seeding!")
